59f3110771b0f22d9ddd677b4d336a809ee31210
MuhammadElkotb <muhammmad175ib59@gmail.com>
2022-10-14 13:59:38 +0200

    bug is now killed

---
+++ agent.py
@@ -0,0 +1,314 @@
+"""
+8 Puzzle Agent
+"""
+import copy
+import math
+from queue import PriorityQueue
+
+import heapdict
+
+EMPTY = ""
+
+
+class State():
+    """
+    State of puzzle
+    """
+
+    def __init__(self, grid, parent_state):
+        self.grid = grid
+        self.parent_state = parent_state
+        self.distance = math.inf
+        self.stringFormat = ""
+        for i in range(3):
+            for j in range(3):
+                self.stringFormat += self.grid[i][j]
+
+
+class StackFrontier:
+    """
+    Frontier used for DFS search
+    """
+
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, state):
+        self.frontier.append(state)
+
+    def pop(self):
+        returned_state = self.frontier[-1]
+        self.frontier = self.frontier[:-1]
+        return returned_state
+
+    def not_empty(self):
+        return len(self.frontier) > 0
+
+
+class QueueFrontier:
+    """
+    Fringe used for BFS search
+    """
+
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, state):
+        self.frontier.append(state)
+
+    def pop(self):
+        returned_state = self.frontier[0]
+        self.frontier = self.frontier[1:]
+        return returned_state
+
+    def not_empty(self):
+        return len(self.frontier) > 0
+
+
+def empty_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+[EMPTY, EMPTY, EMPTY],
+[EMPTY, EMPTY, EMPTY]]
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [["1", "0", "2"],
+            ["3", "4", "5"],
+            ["6", "7", "8"]]
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board and the zero.
+    """
+    possible_moves = set()
+    zero = tuple()
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[i][j] == '0':
+                zero = (i, j)
+                if allowed_action(i + 1, j):
+                    possible_moves.add((i + 1, j))
+                if allowed_action(i - 1, j):
+                    possible_moves.add((i - 1, j))
+                if allowed_action(i, j + 1):
+                    possible_moves.add((i, j + 1))
+                if allowed_action(i, j - 1):
+                    possible_moves.add((i, j - 1))
+                break
+    if len(possible_moves) == 0:
+        return None
+    else:
+        return possible_moves, zero
+
+
+def result(board, action, zero):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    new_board = empty_state()
+    for i in range(0, 3):
+        for j in range(0, 3):
+            new_board[i][j] = copy.deepcopy(board[i][j])
+
+    if new_board[zero[0]][zero[1]] != '0':
+        raise Exception
+    else:
+        temp = new_board[action[0]][action[1]]
+        new_board[action[0]][action[1]] = new_board[zero[0]][zero[1]]
+        new_board[zero[0]][zero[1]] = temp
+
+        return new_board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    if (board[0][0] == "0" and board[0][1] == "1" and board[0][2] == "2"
+        and board[1][0] == "3" and board[1][1] == "4" and board[1][2] == "5"
+        and board[2][0] == "6" and board[2][1] == "7" and board[2][2] == "8"):
+        return True
+    else:
+        return False
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    return winner(board)
+
+
+def back_track(state):
+    """
+    :param state: Winner (final) state of the puzzle
+    :return: Stack containing the parent chain starting from final state ending at initial state
+    """
+    stack = []
+    stack.append(state)
+    parent = state.parent_state
+    while parent is not None:
+        stack.append(parent)
+        parent = parent.parent_state
+    stack.pop()
+    return stack
+
+
+def good_print(grid):
+    """
+    :param grid: Grid of the puzzle
+    prints the grid
+    """
+    for row in grid:
+        for element in row:
+            print(element, end=" ")
+        print("\n", end="")
+    print("\n", end="")
+
+
+def isPresent(state, list_of_states):
+    """
+    :param state: State of the puzzle
+    :param list_of_states: List to be searched in
+    :return: true if the state already exists in the list
+    """
+    print(any(l.grid == state.grid for l in list_of_states))
+    return any(l.grid == state.grid for l in list_of_states)
+
+
+def allowed_action(i, j):
+    """
+
+    :param i: X Coordinate of the cell to be swapped with empty cell
+    :param j: X Coordinate of the cell to be swapped with empty cell
+    :return: true if the cell is within the constraints of the puzzle
+    """
+    return 0 <= i < 3 and 0 <= j < 3
+
+
+class ManhattanDistance:
+
+    def distance(self, index: str, x2, y2):
+        x1 = int(int(index) / 3)
+        y1 = int(index) % 3
+        return abs(x1 - x2) + abs(y1 - y2)
+
+    def calculate(self, state: State):
+        sum = 0
+        for i in range(3):
+            for j in range(3):
+                sum += self.distance(state.grid[i][j], i, j)
+        return sum
+
+
+class EuclidianDistance:
+
+    def distance(self, index: str, x2, y2):
+        x1 = int(int(index) / 3)
+        y1 = int(index) % 3
+        return math.sqrt(((x1 - x2) ** 2 + (y1 - y2) ** 2))
+
+    def calculate(self, state: State):
+        sum = 0
+        for i in range(3):
+            for j in range(3):
+                sum += self.distance(state.grid[i][j], i, j)
+        return sum
+
+
+def calculateHeruistic(state: State, function):
+    return function.calculate(state)
+
+
+def DFS(board):
+    """
+    :param board: starting state grid of the puzzle
+    :return Searches for the way to solve the puzzle
+    and returns stack of states leading to the solution
+    starting at final state (Goal) and ending at initial_state
+    """
+    frontier = StackFrontier()
+    visited_states = set()
+    init_state = State(board, None)
+    frontier.add(init_state)
+    while frontier.not_empty():
+        state = frontier.pop()
+        visited_states.add(state.stringFormat)
+        if winner(state.grid):
+            print(state.grid)
+            print(state.parent_state.grid)
+            return back_track(state)
+
+        set_of_actions, zero = actions(state.grid)
+        for action in set_of_actions:
+            next_state = State(result(state.grid, action, zero), state)
+            if not (next_state.stringFormat in visited_states):
+                frontier.add(next_state)
+
+    return None
+
+
+def BFS(board):
+    """
+    :param board: starting state grid of the puzzle
+    :return Searches for the way to solve the puzzle
+    and returns stack of states leading to the solution
+    starting at final state (Goal) and ending at initial_state
+    """
+    frontier = QueueFrontier()
+    visited_states = set()
+    init_state = State(board, None)
+    frontier.add(init_state)
+    while frontier.not_empty():
+        state = frontier.pop()
+        visited_states.add(state.stringFormat)
+        if winner(state.grid):
+            print(state.grid)
+            print(state.parent_state.grid)
+            return back_track(state)
+
+        set_of_actions, zero = actions(state.grid)
+        for action in set_of_actions:
+            next_state = State(result(state.grid, action, zero), state)
+            if not (next_state.stringFormat in visited_states):
+                frontier.add(next_state)
+
+    return None
+
+
+def AStar(board, function):
+    """
+    Returns the next action for the current state on the board.
+    """
+    frontier = heapdict.heapdict()
+    visited_states = set()
+    init_state = State(board, None)
+    init_state.distance = 0
+    frontier[init_state] = calculateHeruistic(init_state, function)
+    while frontier.__len__() > 0:
+        state, dist = frontier.popitem()
+        visited_states.add(state.stringFormat)
+
+        if winner(state.grid):
+            print(state.grid)
+            print(state.parent_state.grid)
+            return back_track(state)
+
+        set_of_actions, zero = actions(state.grid)
+
+        for action in set_of_actions:
+            next_state = State(result(state.grid, action, zero), state)
+            if not (next_state.stringFormat in visited_states):
+                heuristic = calculateHeruistic(next_state, function)
+                if (dist + 1 <= next_state.distance):
+                    next_state.parent_state = state
+                    next_state.distance = dist + 1
+                frontier[next_state] = next_state.distance + heuristic
+    return None
---
+++ puzzle.py
@@ -0,0 +1,143 @@
+import sys
+import time
+
+import pygame
+
+import agent as ai
+import builder
+
+pygame.init()
+size = width, height = 600, 400
+
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+screen = pygame.display.set_mode(size)
+
+# Fonts
+smallFont = pygame.font.Font("OpenSans-Regular.ttf", 20)
+mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
+largeFont = pygame.font.Font("OpenSans-Regular.ttf", 40)
+moveFont = pygame.font.Font("OpenSans-Regular.ttf", 60)
+
+begin = False
+stack = None
+board = ai.initial_state()
+button_builder = builder.ButtonBuilder(screen, pygame)
+
+manhattanDistance = ai.ManhattanDistance()
+euclidianDistance = ai.EuclidianDistance()
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill(black)
+
+    # Let user start using the agent.
+    if begin is False:
+
+        # Draw title
+        title = largeFont.render("8-Puzzle Agent", True, white)
+        titleRect = title.get_rect()
+        titleRect.center = ((width / 2), 50)
+        screen.blit(title, titleRect)
+
+        # Draw start buttons
+        button_builder.specify_dimensions(60, (height / 2), width / 4, 30)
+        button_builder.specify_colors(white, black)
+        button_builder.specify_text("Solve DFS", smallFont)
+        solve_DFS_button = button_builder.build()
+
+        button_builder.specify_dimensions((width / 2) - 80, (height / 2), width / 4, 30)
+        button_builder.specify_colors(white, black)
+        button_builder.specify_text("Solve BFS", smallFont)
+        solve_BFS_button = button_builder.build()
+
+        button_builder.specify_dimensions((width / 2) + 80, (height / 2), width / 4, 30)
+        button_builder.specify_colors(white, black)
+        button_builder.specify_text("Solve A*", smallFont)
+        solve_astar_button = button_builder.build()
+
+        # Check if button is clicked
+        click, _, _ = pygame.mouse.get_pressed()
+        if click == 1:
+            mouse = pygame.mouse.get_pos()
+            if solve_DFS_button.collidepoint(mouse):
+                stack = ai.DFS(board)
+                time.sleep(0.2)
+                begin = True
+
+            elif solve_BFS_button.collidepoint(mouse):
+                stack = ai.BFS(board)
+                time.sleep(0.2)
+                begin = True
+
+            elif solve_astar_button.collidepoint(mouse):
+                stack = ai.AStar(board, manhattanDistance)
+                time.sleep(0.2)
+                begin = True
+    else:
+        # Draw game board
+        tile_size = 80
+        tile_origin = (width / 2 - (1.5 * tile_size),
+                height / 2 - (1.5 * tile_size))
+        tiles = []
+        for i in range(3):
+            row = []
+            for j in range(3):
+                rect = pygame.Rect(
+                    tile_origin[0] + j * tile_size,
+                    tile_origin[1] + i * tile_size,
+                    tile_size, tile_size
+                )
+                pygame.draw.rect(screen, white, rect, 3)
+
+                if board[i][j] != ai.EMPTY:
+                    cell = moveFont.render(board[i][j], True, white)
+                    cellRect = cell.get_rect()
+                    cellRect.center = rect.center
+                    screen.blit(cell, cellRect)
+                row.append(rect)
+            tiles.append(row)
+
+        the_end = ai.terminal(board)
+
+        # Draw step button
+        button_builder.specify_dimensions((width / 4), 10, width / 2, 50)
+        button_builder.specify_colors(white, black)
+        button_builder.specify_text("let the agent take a move", smallFont)
+        step_button = button_builder.build()
+
+        # Find solution and get steps stack
+        # Check for AI move
+        # Check if step button is clicked
+        click, _, _ = pygame.mouse.get_pressed()
+        if click == 1:
+            mouse = pygame.mouse.get_pos()
+            if step_button.collidepoint(mouse) and not the_end:
+                # if stack == None :
+                # To be implemented
+                # display a message saying no solution to this puzzle
+                time.sleep(0.5)
+                board = stack[-1].grid
+                stack.pop()
+        if the_end:
+            button_builder.specify_dimensions(width / 3, height - 65, width / 3, 50)
+            button_builder.specify_colors(white, black)
+            button_builder.specify_text("Start Again", mediumFont)
+            again_button = button_builder.build()
+
+            click, _, _ = pygame.mouse.get_pressed()
+            if click == 1:
+                mouse = pygame.mouse.get_pos()
+                if again_button.collidepoint(mouse):
+                    time.sleep(0.2)
+                    board = ai.initial_state()
+                    begin = False
+
+    pygame.display.flip()
+    
