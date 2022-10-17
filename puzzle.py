9200ef254004525fa087d53e7dbb1f31ff1c0de8
Deffo0 <73228199+Deffo0@users.noreply.github.com>
2022-10-17 20:13:09 +0200

    undo feature + selecting the heuristics

--- puzzle.py
+++ puzzle.py
@@ -12,21 +12,24 @@ size = width, height = 600, 400
 # Colors
 black = (0, 0, 0)
 white = (255, 255, 255)
+gold = (253, 169, 19)
+sun = (253, 169, 19)
 
 screen = pygame.display.set_mode(size)
 
 # Fonts
 
-small_font = pygame.font.Font("OpenSans-Regular.ttf", 20)
-medium_font = pygame.font.Font("OpenSans-Regular.ttf", 28)
-large_font = pygame.font.Font("OpenSans-Regular.ttf", 40)
-cell_font = pygame.font.Font("OpenSans-Regular.ttf", 60)
+small_font = pygame.font.Font("FuzzyBubbles-Regular.ttf", 20)
+medium_font = pygame.font.Font("FuzzyBubbles-Regular.ttf", 28)
+large_font = pygame.font.Font("FuzzyBubbles-Regular.ttf", 40)
+cell_font = pygame.font.Font("FuzzyBubbles-Regular.ttf", 60)
 
 begin = False
 stack = None
+undo_stack = []
 board = ai.initial_state()
-button_builder = builder.ButtonBuilder(screen, pygame)
 
+button_builder = builder.ButtonBuilder(screen, pygame)
 title_builder = builder.TitleBuilder(screen, pygame)
 board_builder = builder.BoardBuilder(screen, pygame)
 
@@ -47,28 +50,30 @@ while True:
         # Draw title
 
         title_builder.specify_dimensions((width / 2), 50)
-        title_builder.specify_colors(white)
+        title_builder.specify_colors(gold)
         title_builder.specify_text("8-Puzzle Agent", large_font)
         title_builder.build()
 
         # Draw start buttons
-        button_builder.specify_dimensions(60, (height / 2), width / 4, 30)
-        button_builder.specify_colors(white, black)
-
+        button_builder.specify_dimensions(130, (height / 2), width / 4, 30)
+        button_builder.specify_colors(gold, black)
         button_builder.specify_text("Solve DFS", small_font)
         solve_DFS_button = button_builder.build()
 
-        button_builder.specify_dimensions((width / 2) - 80, (height / 2), width / 4, 30)
-        button_builder.specify_colors(white, black)
-
+        button_builder.specify_dimensions(340, (height / 2), width / 4, 30)
+        button_builder.specify_colors(gold, black)
         button_builder.specify_text("Solve BFS", small_font)
         solve_BFS_button = button_builder.build()
 
-        button_builder.specify_dimensions((width / 2) + 80, (height / 2), width / 4, 30)
-        button_builder.specify_colors(white, black)
+        button_builder.specify_dimensions(130, (height / 2) + 40, width / 2 + 60, 30)
+        button_builder.specify_colors(gold, black)
+        button_builder.specify_text("Solve A* with manhattan heuristics", small_font)
+        solve_astar_manhattan_button = button_builder.build()
 
-        button_builder.specify_text("Solve A*", small_font)
-        solve_astar_button = button_builder.build()
+        button_builder.specify_dimensions(130, (height / 2) + 80, width / 2 + 60, 30)
+        button_builder.specify_colors(gold, black)
+        button_builder.specify_text("Solve A* with euclidian heuristics", small_font)
+        solve_astar_euclidian_button = button_builder.build()
 
         # Check if button is clicked
         click, _, _ = pygame.mouse.get_pressed()
@@ -84,15 +89,20 @@ while True:
                 time.sleep(0.2)
                 begin = True
 
-            elif solve_astar_button.collidepoint(mouse):
+            elif solve_astar_manhattan_button.collidepoint(mouse):
                 stack = ai.AStar(board, manhattanDistance)
+                time.sleep(0.2)
+                begin = True
+
+            elif solve_astar_euclidian_button.collidepoint(mouse):
+                stack = ai.AStar(board, euclidianDistance)
                 time.sleep(0.2)
                 begin = True
     else:
         # Draw game board
 
         board_builder.specify_dimensions((width / 2 - (1.5 * 80), height / 2 - (1.5 * 80)), 80)
-        board_builder.specify_colors(white)
+        board_builder.specify_colors(gold)
         board_builder.specify_board(board, cell_font)
         board_builder.build()
 
@@ -100,26 +110,48 @@ while True:
 
         # Draw step button
         button_builder.specify_dimensions((width / 4), 10, width / 2, 50)
-        button_builder.specify_colors(white, black)
+        button_builder.specify_colors(gold, black)
         button_builder.specify_text("let the agent take a move", small_font)
         step_button = button_builder.build()
+
+        # Draw undo button
+        button_builder.specify_dimensions((width - 100), 120, 60, 30)
+        button_builder.specify_colors(gold, black)
+        button_builder.specify_text("undo", small_font)
+        undo_button = button_builder.build()
         # Find solution and get steps stack
         # Check for AI move
         # Check if step button is clicked
         click, _, _ = pygame.mouse.get_pressed()
         if click == 1:
             mouse = pygame.mouse.get_pos()
-            if step_button.collidepoint(mouse) and not the_end:
-                # if stack == None :
-                # To be implemented
-                # display a message saying no solution to this puzzle
+            if step_button.collidepoint(mouse) and not the_end and len(stack):
                 time.sleep(0.1)
+                if board == stack[-1].grid:
+                    board = stack[-1].grid
+                    undo_stack.append(stack[-1])
+                    stack.pop()
+
                 board = stack[-1].grid
+                undo_stack.append(stack[-1])
                 stack.pop()
+
+            elif undo_button.collidepoint(mouse) and len(undo_stack):
+                time.sleep(0.1)
+                if board == undo_stack[-1].grid:
+                    board = undo_stack[-1].grid
+                    stack.append(undo_stack[-1])
+                    undo_stack.pop()
+                    the_end = False
+
+                board = undo_stack[-1].grid
+                stack.append(undo_stack[-1])
+                undo_stack.pop()
+                the_end = False
+
         if the_end:
             button_builder.specify_dimensions(width / 3, height - 65, width / 3, 50)
-            button_builder.specify_colors(white, black)
-
+            button_builder.specify_colors(gold, black)
             button_builder.specify_text("Start Again", medium_font)
             again_button = button_builder.build()
 
