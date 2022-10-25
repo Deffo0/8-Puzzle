45960ddb1503a6f51ececd1d03d737d31898de15
Deffo0 <73228199+Deffo0@users.noreply.github.com>
2022-10-25 15:12:44 +0200

    pipline

--- Testing/file_writer.py
+++ Testing/file_writer.py
@@ -1,24 +1,30 @@
-def write_to_file_fs(grid, cost_and_depth, actions_to_solve, explored, fringe_max_size, fringe_size, max_depth, run_time, expected):
+def write_to_file_fs(grid, cost_and_depth, actions_to_solve, explored, fringe_max_size, fringe_size, max_depth,
+                     run_time, expected):
     def get_file_name(path_name):
         file = open(path_name, 'a')
         file.writelines(
             [f"{grid} \n", f"Actions to do: {actions_to_solve}\n", f"cost-to-goal: {cost_and_depth}\n",
              f"depth-to-goal: {cost_and_depth} \n",
-             f"nodes explored: {explored}\n", f"Max. fringe size: {fringe_max_size} \n", f"Fringe size: {fringe_size} \n",
+             f"nodes explored: {explored}\n", f"Max. fringe size: {fringe_max_size} \n",
+             f"Fringe size: {fringe_size} \n",
              f"Max. depth: {max_depth} \n", f"running time: {run_time} seconds \n",
              "solvable" if expected else "unsolvable", "\n\n"])
         file.close()
+
     return get_file_name
 
 
-def write_to_file_star(grid, cost_and_depth, actions_to_solve, explored, fringe_max_size, fringe_size, run_time, expected):
+def write_to_file_star(grid, cost_and_depth, actions_to_solve, explored, fringe_max_size, fringe_size, run_time,
+                       expected):
     def get_file_name(path_name):
         file = open(path_name, 'a')
         file.writelines(
             [f"{grid} \n", f"Actions to do: {actions_to_solve}\n", f"cost-to-goal: {cost_and_depth}\n",
              f"depth-to-goal: {cost_and_depth} \n",
-             f"nodes explored: {explored}\n", f"Max. fringe size: {fringe_max_size} \n", f"Fringe size: {fringe_size} \n",
+             f"nodes explored: {explored}\n", f"Max. fringe size: {fringe_max_size} \n",
+             f"Fringe size: {fringe_size} \n",
              f"running time: {run_time} seconds \n",
              "solvable" if expected else "unsolvable", "\n\n"])
         file.close()
+
     return get_file_name
